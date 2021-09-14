e100bd8e9e2c161e3265ae046638c90080f0b356
Takuma-86 <satotaT7335@gmail.com>
2021-09-14 21:53:15 +0900

    modified

--- gakusai2021.py
+++ gakusai2021.py
@@ -1,5 +1,5 @@
 # @Date:   2021-09-12T18:09:19+09:00
-# @Last modified time: 2021-09-13T13:28:36+09:00
+# @Last modified time: 2021-09-14T19:59:44+09:00
 
 
 
@@ -14,11 +14,11 @@ GPIO.setup(22,GPIO.OUT)
 GPIO.setup(5,GPIO.OUT)
 GPIO.setup(25,GPIO.OUT)
 
-pwma = GPIO.PWM(22,50)
-pwmb = GPIO.PWM(25,50)
+rightMotorPwm = GPIO.PWM(22,50)
+leftMotorPwm = GPIO.PWM(25,50)
 
-pwma.start(0)
-pwmb.start(0)
+rightMotorPwm.start(0)
+leftMotorPwm.start(0)
 
 pi = pigpio.pi()
 pi.setmode(19,pigpio.OUTPUT)
@@ -41,24 +41,26 @@ def　right_rotation(): #砲塔の右旋回
     if pulse < 240:
         pulse += 19
         pi.set_PWM_dutycycle(19,pulse)
+        time.sleep(10**(-6))
         return
 
 def left_rotation(): #砲塔の左旋回
     if pulse > 50:
         pulse -= 19
-        pi.set_PWM_dutycycle(19.pulse)
+        pi.set_PWM_dutycycle(19,pulse)
+        time.sleep(10**(-6))
         return
 
 #dcモーター１が右前輪、dcモーター２が左前輪
 def stop():
-    pwma.ChangeDutyCycle(0)
-    pwmb.ChangeDutyCycle(0)
+    rightMotorPwm.ChangeDutyCycle(0)
+    leftMotorPwm.ChangeDutyCycle(0)
     time.sleep(0.1)
     return
 
 def straight():
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.LOW)
@@ -67,8 +69,8 @@ def straight():
     return
 
 def right():
-    pwma.ChangeDutyCycle(50)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(50)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.LOW)
@@ -77,8 +79,8 @@ def right():
     return
 
 def left():
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(50)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(50)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.LOW)
@@ -87,8 +89,8 @@ def left():
     return
 
 def back():
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.HIGH)
@@ -97,8 +99,8 @@ def back():
     return
 
 def back_right():
-    pwma.ChangeDutyCycle(50)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(50)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.HIGH)
@@ -107,8 +109,8 @@ def back_right():
     return
 
 def back_left():
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(50)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(50)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.HIGH)
@@ -117,8 +119,8 @@ def back_left():
     return
 
 def right_turn(): #右旋回
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.HIGH)
@@ -127,8 +129,8 @@ def right_turn(): #右旋回
     return
 
 def left_turn(): #左旋回
-    pwma.ChangeDutyCycle(100)
-    pwmb.ChangeDutyCycle(100)
+    rightMotorPwm.ChangeDutyCycle(100)
+    leftMotorPwm.ChangeDutyCycle(100)
     GPIO.output(22,GPIO.HIGH)
     GPIO.output(25,GPIO.HIGH)
     GPIO.output(27,GPIO.LOW)
