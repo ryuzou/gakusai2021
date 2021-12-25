e0ea8a1cff51a14eb50be5a980e9912405df42cd
ryuzou <ryuzot.1112@gmail.com>
2021-12-25 09:11:27 +0900

    for NHK

--- Client/main.py
+++ Client/main.py
@@ -15,8 +15,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 def update_controller(joystick_data):
-    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}, "shot_button": 0, "reload_button": 0, "left": 0,
-                "right": 0}
+    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}}
 
     # ジョイスティックの初期化
     pygame.joystick.init()
@@ -56,120 +55,32 @@ def update_controller(joystick_data):
                 else:
                     joystick_stick_degree = int(math.degrees(rad))
                 # print(f"θ={joystick_stick_degree},r={joystick_radius},x={x},y={y}")
-            elif e.type == pygame.locals.JOYBUTTONDOWN:
-                if e.button == 1:
-                    shot_button = 1
-                elif e.button == 0:
-                    reload_button = 1
-                elif e.button == 4:
-                    left = 1
-                elif e.button == 5:
-                    right = 1
-            elif e.type == pygame.locals.JOYBUTTONUP:
-                if e.button == 1:
-                    shot_button = 0
-                elif e.button == 0:
-                    reload_button = 0
-                elif e.button == 4:
-                    left = 0
-                elif e.button == 5:
-                    right = 0
 
             # JSON書き込み
             (gameDict["joystick"])["radius"] = joystick_radius
             (gameDict["joystick"])["stick_degree"] = joystick_stick_degree
-            gameDict["reload_button"] = reload_button
-            gameDict["shot_button"] = shot_button
-            gameDict["left"] = left
-            gameDict["right"] = right
             joystick_data.updateData(gameDict)
 
-def imageGetter():
-
-    baseImg = np.zeros((480, 640, 3)).astype('int')
-    baseImg += [0, 0, 255][::-1]  # RGBで青指定
-
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 5]
-    result, encimg = cv2.imencode('.jpg', baseImg, encode_param)
-    if False == result:
-        print('could not encode image!')
-        exit()
-
-    decimg = cv2.imdecode(encimg, cv2.IMREAD_COLOR)
-    baseImg = decimg
-
-    PORT_UDP = 8092
-
-    socket_udp = socket(AF_INET, SOCK_DGRAM)
-    socket_udp.bind(("0.0.0.0", PORT_UDP))
-
-    buff = 1034 * 64
-
-    imagePartDict = {}
-
-    initialised_flag = False
-
-    while True:
-        data, _ = socket_udp.recvfrom(buff)
-        data = data.decode()
-
-        header = data.split("&")[0]
-        x_cod = int(header.split("_")[0])
-        y_cod = int(header.split("_")[1])
-
-        if initialised_flag == False:
-            x_len = int(header.split("_")[2])
-            y_len = int(header.split("_")[3])
-            initialised_flag = True
-
-
-        timestamp = int(header.split("_")[4])
-        xycodOD = str(x_cod) + str(y_cod)
-
-        runFlag = True
-        if xycodOD in imagePartDict:
-            if imagePartDict[xycodOD] >= timestamp:
-                runFlag = False
-            else:
-                imagePartDict[xycodOD] = timestamp
-        else:
-            imagePartDict[xycodOD] = timestamp
-
-        #print(data)
-
-        if runFlag:
-            img_data = base64.b64decode(data.split("&")[1])
-            nparr = np.frombuffer(img_data, np.uint8)
-            img_decode = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-            baseImg[y_cod:(y_cod + y_len), x_cod:(x_cod + x_len)] = img_decode
-            cv2.imshow("img", baseImg)
-        if cv2.waitKey(1) != -1:
-            break
-
-
 def json_sender(joystick_data):
-    IPADDR = "192.168.0.3"
+    IPADDR = "192.168.0.7"
     PORT_TCP = 8000
     socket_tcp = socket(AF_INET, SOCK_STREAM)
     socket_tcp.connect((IPADDR, PORT_TCP))
-    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}, "shot_button": 0, "reload_button": 0, "left": 0,
-                "right": 0}
+    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}}
     baseGameJson = joystick_data.getData()
     global gameJson
     while True:
-        socket_tcp.send(((baseGameJson + '\n').encode("utf-8")))
-        print(baseGameJson)
+        socket_tcp.send(((str(str(baseGameJson['joystick']['radius']) + ',' + str(baseGameJson['joystick']['stick_degree'])) + '\n').encode("utf-8")))
         baseGameJson = joystick_data.getData()
 
 class JoystickData:
     lock = threading.Lock()
-    gameJson = json.dumps({"joystick": {"radius": 0, "stick_degree": 0}, "shot_button": 0, "reload_button": 0, "left": 0, "right": 0})
+    gameJson = {"joystick": {"radius": 0, "stick_degree": 0}}
 
     def updateData(self, dataDict):
-        dataJson = json.dumps(dataDict)
+        dataJson = dataDict
         with self.lock:
-            self.gameJson = dataJson
+            self.gameJson = dataDict
 
     def getData(self):
         with self.lock:
@@ -181,9 +92,8 @@ def main():
     with ThreadPoolExecutor() as executor:
         thread1 = executor.submit(update_controller, joystick_data)
         thread2 = executor.submit(json_sender, joystick_data)
-        thread3 = executor.submit(imageGetter)
 
-        features = [thread1, thread2, thread3]
+        features = [thread1, thread2]
 
 if __name__ == '__main__':
     main()
