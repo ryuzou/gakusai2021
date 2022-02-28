cfec08bb92562dc66eb3b4d490ad4c95efd268cb
ryuzou <ryuzot.1112@gmail.com>
2022-02-28 14:07:20 +0900

    changes

--- Client/main.py
+++ Client/main.py
@@ -14,8 +14,11 @@ from pygame import locals
 from concurrent.futures import ThreadPoolExecutor
 
 
-def update_controller(joystick_data):
-    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}}
+def main():
+    IPADDR = "192.168.0.4"
+    PORT_TCP = 8000
+    socket_tcp = socket(AF_INET, SOCK_STREAM)
+    socket_tcp.connect((IPADDR, PORT_TCP))
 
     # ジョイスティックの初期化
     pygame.joystick.init()
@@ -54,46 +57,10 @@ def update_controller(joystick_data):
                     joystick_stick_degree = int(math.degrees(rad) + 360)
                 else:
                     joystick_stick_degree = int(math.degrees(rad))
-                # print(f"θ={joystick_stick_degree},r={joystick_radius},x={x},y={y}")
+                print(f"θ={joystick_stick_degree},r={joystick_radius},x={x},y={y}")
+                socket_tcp.send(((str(joystick_radius) + ',' + str(joystick_stick_degree)) + '\n').encode("utf-8"))
 
-            # JSON書き込み
-            (gameDict["joystick"])["radius"] = joystick_radius
-            (gameDict["joystick"])["stick_degree"] = joystick_stick_degree
-            joystick_data.updateData(gameDict)
 
-def json_sender(joystick_data):
-    IPADDR = "192.168.0.7"
-    PORT_TCP = 8000
-    socket_tcp = socket(AF_INET, SOCK_STREAM)
-    socket_tcp.connect((IPADDR, PORT_TCP))
-    gameDict = {"joystick": {"radius": 0, "stick_degree": 0}}
-    baseGameJson = joystick_data.getData()
-    global gameJson
-    while True:
-        socket_tcp.send(((str(str(baseGameJson['joystick']['radius']) + ',' + str(baseGameJson['joystick']['stick_degree'])) + '\n').encode("utf-8")))
-        baseGameJson = joystick_data.getData()
-
-class JoystickData:
-    lock = threading.Lock()
-    gameJson = {"joystick": {"radius": 0, "stick_degree": 0}}
-
-    def updateData(self, dataDict):
-        dataJson = dataDict
-        with self.lock:
-            self.gameJson = dataDict
-
-    def getData(self):
-        with self.lock:
-            ret = self.gameJson
-        return ret
-
-def main():
-    joystick_data = JoystickData()
-    with ThreadPoolExecutor() as executor:
-        thread1 = executor.submit(update_controller, joystick_data)
-        thread2 = executor.submit(json_sender, joystick_data)
-
-        features = [thread1, thread2]
 
 if __name__ == '__main__':
     main()
